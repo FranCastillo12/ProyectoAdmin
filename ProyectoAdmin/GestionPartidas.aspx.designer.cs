09cf04b9b1c3969616874137f9fd72b75def2205
Brian <briangrji28@gmail.com>
2023-07-28 16:25:11 -0600

    Correción de navbar

--- ProyectoAdmin/GestionPartidas.aspx.designer.cs
+++ ProyectoAdmin/GestionPartidas.aspx.designer.cs
@@ -15,6 +15,24 @@ namespace ProyectoAdmin
     {
 
         /// <summary>
+        /// bntCrearMeta control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button bntCrearMeta;
+
+        /// <summary>
+        /// btnAsignarPresupuesto control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAsignarPresupuesto;
+
+        /// <summary>
         /// containerCrearPartida control.
         /// </summary>
         /// <remarks>
