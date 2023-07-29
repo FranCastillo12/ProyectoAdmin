373a0f392c27a4fe480969b3c7333f55f3469490
Francisco Castillo <castillof075@gmail.com>
2023-07-29 16:02:36 -0600

    Clases de negocios para llamar base de datos

---
+++ Negocios/Neg_GestionPartidas.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocios
+{
+    public class Neg_GestionPartidas
+    {
+
+
+
+
+
+
+
+        #region "Francisco"
+
+        public DataTable MetasXDepartamento(string idDepartamento)
+        {
+            string spName = "";
+            var lstParametros = new List<SqlParameter>()
+            {
+               new SqlParameter("@idDepartamento", idDepartamento),
+
+            };
+            Datos.ConexionSQL iConexion = new Datos.ConexionSQL();
+            return iConexion.ExecuteSPWithDT(spName, lstParametros);
+
+        }
+        public DataTable PartidasXDepartamento(string idDepartamento,string meta)
+        {
+            string spName = "";
+            var lstParametros = new List<SqlParameter>()
+            {
+               new SqlParameter("@idDepartamento", idDepartamento),
+              new SqlParameter("@meta", meta)
+
+            };
+            Datos.ConexionSQL iConexion = new Datos.ConexionSQL();
+            return iConexion.ExecuteSPWithDT(spName, lstParametros);
+
+        }
+
+        public void AsignarPresupuesto(string meta,string partida,string departamento,string presupuesto)
+        {
+            string spName = "";
+            var lstParametros = new List<SqlParameter>()
+            {
+               new SqlParameter("@meta", meta),
+              new SqlParameter("@partida", partida),
+              new SqlParameter("@departamento", departamento),
+              new SqlParameter("@presupuesto", presupuesto),
+
+            };
+            Datos.ConexionSQL iConexion = new Datos.ConexionSQL();
+            iConexion.ExecuteSPWithDT(spName, lstParametros);
+        }
+
+
+
+
+        #endregion
+    }
+}
