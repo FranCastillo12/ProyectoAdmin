39bfa4eef8223993dfb51b4044cd200d1bec9a7e
Francisco Castillo <castillof075@gmail.com>
2023-07-28 18:46:21 -0600

    Se agrega la funcionalidad de asignar presupuesto

---
+++ ProyectoAdmin/Solicitud_gasto_presupuesto.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProyectoAdmin
+{
+    public partial class Solicitud_gasto_presupuesto : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- ProyectoAdmin/Solicitud_gasto_presupuesto.aspx.designer.cs
+++ ProyectoAdmin/Solicitud_gasto_presupuesto.aspx.designer.cs
@@ -11,8 +11,17 @@ namespace ProyectoAdmin
 {
 
 
-    public partial class GestionPartidas
+    public partial class Solicitud_gasto_presupuesto
     {
+
+        /// <summary>
+        /// Control fecha_actual.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl fecha_actual;
 
         /// <summary>
         /// Control txt_monto_solicitado.
@@ -22,6 +31,15 @@ namespace ProyectoAdmin
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txt_monto_solicitado;
+
+        /// <summary>
+        /// Control txt_motivos_solicitud.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_motivos_solicitud;
 
         /// <summary>
         /// Control btn_enviar_solicitud.
